8833cda0e49c6ad2147cd2680eef46d94d2d45bd
mambisi <lilbizi96@gmail.com>
2021-04-29 09:37:18 +0000

    change async to sync

--- examples/simple_price.rs
+++ examples/simple_price.rs
@@ -1,9 +1,7 @@
 use coingecko::{Client, SimplePriceReq};
 
 pub fn main() {
-    smol::block_on(async {
-        let client = Client::new().unwrap();
-        let req = SimplePriceReq::new("ethereum".into(), "eur,gbp,usd".into());
-        println!("{:#?}", client.simple_price(req));
-    })
+    let client = Client::new().unwrap();
+    let req = SimplePriceReq::new("ethereum".into(), "eur,gbp,usd".into());
+    println!("{:#?}", client.simple_price(req).unwrap().get("ethereum"));
 }
